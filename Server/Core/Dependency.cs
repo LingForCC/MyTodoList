5351c0a7a78d674672f55497e9f3805c2ea46c58
Colin Liu <colin198702@gmail.com>
2019-04-28 23:02:37 +0800

    Create initial Task Object

---
+++ Server/Core/Dependency.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Core
+{
+    public class Dependency
+    {
+        public Dependency()
+        {
+        }
+    }
+}
---
+++ Server/Core/Task.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core
+{
+    public class Task
+    {
+        private List<Dependency> _dependencies;
+
+        public Task()
+        {
+            _dependencies = new List<Dependency>();
+        }
+
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Description
+        { 
+            get;
+            set;
+        }
+
+        public bool IsCompleted
+        {
+            get;
+            set;
+        }
+
+        public void AddDependency(Dependency dependency)
+        {
+            if(null != dependency)
+            {
+                _dependencies.Add(dependency);
+            }
+            else
+            {
+                throw new TaskException("The input parameter can not be null");
+            }
+        }
+
+    }
+
+    public class TaskException : Exception
+    {
+
+        public TaskException(string message)
+            : this(message, null)
+        {
+
+        }
+    
+
+        public TaskException(string message, Exception innerException) 
+            : base(message, innerException)
+        {
+        }
+    }
+}
