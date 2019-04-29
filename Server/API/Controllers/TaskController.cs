0e1263e1f94c92825b06f5ca162eb9ec47ad56b5
Colin Liu <colin198702@gmail.com>
2019-04-29 23:21:57 +0800

    Add WebAPI skeleton for Server

---
+++ Server/API/Controllers/TaskController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Core;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+
+        // PUT api/values/5
+        [HttpPost("{id}")]
+        public void AddTask(int id, [FromBody] Core.Task task)
+        {
+            //do nothing for now
+        }
+
+    }
+}
