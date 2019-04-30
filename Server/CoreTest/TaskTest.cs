767bdec4ca0b8439567afcdeb4d2594fb637f7f4
Rukai Yu <yurukai@vip.qq.com>
2019-04-30 23:23:31 +0800

    test(task): add unit test

--- Server/CoreTest/TaskTest.cs
+++ Server/CoreTest/TaskTest.cs
@@ -1,4 +1,3 @@
-using System;
 using Xunit;
 using Core;
 
@@ -12,5 +11,24 @@ namespace CoreTest
             Task task = new Task();
             Assert.Throws<TaskException>(() => task.AddDependency(null));
         }
+
+        [Fact]
+        public void TestAddTaskSuccesfully()
+        {
+            Task task = new Task()
+            {
+                Id = 1,
+                Name = "test task",
+            };
+
+            // TODO:
+            // Use moq
+            ITaskRepository repo = new TaskRepository();
+
+            repo.Add(task);
+
+            Assert.Equal(repo.FindById(task.Id).Name, task.Name);
+        }
+
     }
 }
