2187be7b1de454a88801f94d5b77dae27bb537fc
Colin Liu <colin198702@gmail.com>
2019-05-02 23:29:13 +0800

    Add dependency to whatwg-fetch

--- Web/src/modules/Todos/index.js
+++ Web/src/modules/Todos/index.js
@@ -1,18 +1,25 @@
 import ViewModule, { state, action, computed } from '../../lib/baseViewModule';
+import 'whatwg-fetch';
 
 export default class Todos extends ViewModule {
-  @state todos = [];
+  	@state todos = [];
 
-  @action
-  add(text, state) {
-    state.todos.push({
-      text
-    })
-  }
+  	@action
+  	add(text, state) {
+    	this.getRemoteData();
+  	}
 
-  getViewProps() {
-    return {
-      addTodo: text => this.add(text),
-    };
-  }
+  	async getRemoteData() {
+    	const response = await fetch("https://localhost:5001/api/task/5");
+    	const statusText = response.statusText;
+    	this.state.todos.push({
+      		statusText
+    	})
+  	}
+
+	getViewProps() {
+		return {
+		addTodo: text => this.add(text),
+		};
+	}
 }
