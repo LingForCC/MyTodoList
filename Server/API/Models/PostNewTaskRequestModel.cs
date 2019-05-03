d9f4233cb9035740124ce3f3b0b9e5eda1129402
Colin Liu <colin198702@gmail.com>
2019-05-03 14:48:13 +0800

    Remove the constraints on new task request model

--- Server/API/Models/PostNewTaskRequestModel.cs
+++ Server/API/Models/PostNewTaskRequestModel.cs
@@ -4,9 +4,7 @@ namespace API.Models
 {
     public class PostNewTaskRequestModel
     {
-        [Required(ErrorMessage = "invalid task name")]
-        [MaxLength(300, ErrorMessage = "task name should be less than 300 length.")]
-        [RegularExpression(Core.Ultils.Regessions.TASK_NAME_REG, ErrorMessage = "invalid task name")]
+
         public string Name { get; set; }
 
         public string Description { get; set; }
