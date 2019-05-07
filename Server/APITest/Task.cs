5cd14cc141469ffb3ae2be13981a47b65ea66349
Colin Liu <colin198702@gmail.com>
2019-05-07 22:18:42 +0800

    Introduce WebAPI test framework

---
+++ Server/APITest/Task.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using Xunit;
+
+namespace APITest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public async void TestAddTaskWithInvalidCharacterAsName()
+        {
+            using (var client = new TestClientProvider().Client)
+            {
+                var param = Newtonsoft.Json.JsonConvert.SerializeObject(new { Name = "**" });
+                HttpContent contentPost = new StringContent(param, Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync("/api/task", contentPost);
+
+                Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            }
+        }
+
+    }
+}
