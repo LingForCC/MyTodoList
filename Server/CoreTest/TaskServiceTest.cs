588cb74fa0e3f1d0c3f7c6fbb80a3a1ccc7e02b4
Colin Liu <colin198702@gmail.com>
2019-05-07 19:15:01 +0800

    Add ServiceException and corresponding tests

---
+++ Server/CoreTest/TaskServiceTest.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+using Core;
+using Core.Services;
+using System.Linq;
+
+namespace CoreTest
+{
+    public class TaskServiceTest
+    {
+
+        #region Create Task in TaskService
+
+        [Fact]
+        public void TestAddNewTask()
+        {
+            string toAddTaskName = "abc 123";
+            IUnitOfWork unitOfWork = new InMemoryUnitOfWork();
+            TaskService ts = new TaskService(unitOfWork, 
+                unitOfWork.GetRepository<Task>());
+            Task newTask = ts.CreateTask(toAddTaskName);
+            Assert.Equal(newTask.Name, toAddTaskName);
+
+            var taskNum = unitOfWork.GetRepository<Task>()
+                .Find(t => t.Name == newTask.Name).Count();
+            Assert.Equal(1, taskNum);
+        }
+
+
+        [Fact]
+        public void TestAddNewTaskWithInvalidName()
+        {
+            string toAddTaskName = "*23";
+            IUnitOfWork unitOfWork = new InMemoryUnitOfWork();
+            TaskService ts = new TaskService(unitOfWork, 
+                unitOfWork.GetRepository<Task>());
+            Assert.Throws<ServiceException>(() => ts.CreateTask(toAddTaskName));
+        }
+
+        #endregion
+
+    }
+}
