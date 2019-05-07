c8bcf1ad33e866880de0eacc7df207920edbf078
Rukai Yu <yurukai@vip.qq.com>
2019-05-08 00:18:43 +0800

    feat: wrap the api response with standard format

---
+++ Server/API/Middlewares/JsonApiWrapperExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace API.Middlewares
+{
+    public static class JsonApiWrapperExtension
+    {
+        public static IApplicationBuilder UseJsonApiWrapper(this IApplicationBuilder app, string version, string name)
+        {
+            return app.UseMiddleware<JsonApiWrapper>(version, name);
+        }
+    }
+}
