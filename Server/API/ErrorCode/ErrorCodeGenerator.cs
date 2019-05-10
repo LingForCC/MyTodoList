e43925f32234299c0447d5a26e101f5aa3c288f1
Colin Liu <colin198702@gmail.com>
2019-05-11 00:07:09 +0800

    Add inital implementation for ErrorCodeGenerator interface and abstract class

---
+++ Server/API/ErrorCode/ErrorCodeGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Core.Services;
+
+namespace API.ErrorCode
+{
+    public interface IErrorCodeGenerator
+    {
+        string ExceptionFullName
+        {
+            get;
+        }
+
+        string GetErrorCode(ServiceException exception);
+    }
+
+
+    public abstract class AbsErrorCodeGenerator<T> : IErrorCodeGenerator
+        where T : ServiceException
+    {
+
+        public string ExceptionFullName
+        {
+            get
+            {
+                return typeof(T).FullName;
+            }
+        }
+
+        public string GetErrorCode(ServiceException exception)
+        {
+            if (exception is T)
+            {
+                return GetErrorCodeInternal(exception as T);
+            }
+
+            throw new Exception();
+        }
+
+        public abstract string GetErrorCodeInternal(T exception);
+
+    }
+
+}
