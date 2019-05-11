f39974430d09aea677c38911a05432d5387be018
Colin Liu <colin198702@gmail.com>
2019-05-12 00:57:14 +0800

    initial implementation for viewing newly added tasks (#26)

--- Web/src/components/TodosPanel/index.js
+++ Web/src/components/TodosPanel/index.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import Input from '@material-ui/core/Input';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 const styles = theme => ({
   input: {
@@ -9,33 +12,47 @@ const styles = theme => ({
 });
 
 export default ({
-  addTodo
+  addTodo,
+  todos
 }) =>  {
   const [state, setState] = useState({
     taskName: ''
   });
   return (
     <div>
-      <Input
-        value = {state.taskName}
-        inputProps={{
-          'aria-label': 'Description',
-        }}
-        onChange = {e => setState({
-          taskName : e.target.value
-        })}
-      />
-      <Button 
-        variant="contained"
-        color="primary"
-        onClick={() => {
-          addTodo(state.taskName);
-          setState({
-            taskName: ''
-          });
-        }}>
-        Add
-      </Button>
+      <div>
+        <Input
+          value = {state.taskName}
+          inputProps={{
+            'aria-label': 'Description',
+          }}
+          onChange = {e => setState({
+            taskName : e.target.value
+          })}
+        />
+        <Button 
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            addTodo(state.taskName);
+            setState({
+              taskName: ''
+            });
+          }}>
+          Add
+        </Button>
+      </div>
+      <div>
+        <List component="nav">
+          {todos.map((element, index) => 
+            <ListItem 
+              key={index} >
+              <ListItemText 
+                primary= {element.name} />
+            </ListItem>
+          )}
+        </List>
+      </div>
     </div>
   )
 }
