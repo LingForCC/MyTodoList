4013064f6b9a6dfa1ca1b13717c6f3393a22b0d7
Colin Liu <colin198702@gmail.com>
2019-05-12 00:46:26 +0800

    initial implementation for viewing newly added tasks

--- Web/src/modules/Todos/index.js
+++ Web/src/modules/Todos/index.js
@@ -5,29 +5,34 @@ import ViewModule, {
 } from '../../lib/baseViewModule';
 
 export default class Todos extends ViewModule {
-	@state todos = [];
+	@state tasks = [];
 
 	constructor(sdk) {
 		super();
 		this.sdk = sdk;
 	}
 
-	@action
-	add(text, state) {
-		this.addTaskRemote(text);
-	}
-
-	async addTaskRemote(text) {
+	async add(text) {
 		try {
 			const resp = await this.sdk.addTask(text);
-			console.log(resp && resp.data);
+			if(resp && resp.data) {
+				this.addTaskAction(resp.data);
+			}
 			alert(resp.message);
-		} catch (e) {}
+		} catch (e) {
+			console.log(e);
+		}
+	}
+
+	@action
+	addTaskAction(data, state){
+		state.tasks.push(data);
 	}
 
 	getViewProps() {
 		return {
 			addTodo: text => this.add(text),
+			todos: this.state.tasks
 		};
 	}
 }
