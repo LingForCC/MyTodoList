26c35a2d04570eefb911b0a8092531dad9d82762
Colin Liu <colin198702@gmail.com>
2019-05-13 07:46:58 +0800

    Implement the ErrorCodeGenerator (#25)
    * Add inital implementation for ErrorCodeGenerator interface and abstract class
    
    * Add initial implementation for ErrorCodeGeneratorManager
    
    * Add automation tests for ErrorCodeGenerator and ErrorCodeGeneratorManager

---
+++ Server/API/ErrorCode/ErrorCodeGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Core.Services;
+
+namespace API.ErrorCode
+{
+    public interface IErrorCodeGenerator
+    {
+        string ExceptionTypeFullName
+        {
+            get;
+        }
+
+        string GetErrorCode(ServiceException exception);
+    }
+
+
+    public abstract class AbsErrorCodeGenerator<T> : IErrorCodeGenerator
+        where T : ServiceException
+    {
+
+        public string ExceptionTypeFullName
+        {
+            get
+            {
+                return typeof(T).FullName;
+            }
+        }
+
+        public string GetErrorCode(ServiceException exception)
+        {
+            if(null == exception)
+            {
+                throw new ErrorCodeGeneratorException("Input exception can not be null.");
+            }
+
+            if (exception is T)
+            {
+                return GetErrorCodeInternal(exception as T);
+            }
+
+            throw new ErrorCodeGeneratorException("An exception of type " +
+                exception.GetType().FullName + " is sent to ErrorCodeGenerator " +
+                	"for " + ExceptionTypeFullName);
+        }
+
+        public abstract string GetErrorCodeInternal(T exception);
+    }
+
+}
---
+++ Server/API/ErrorCode/ErrorCodeGeneratorException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace API.ErrorCode
+{
+    public class ErrorCodeGeneratorException : Exception
+    {
+        public ErrorCodeGeneratorException() { }
+
+        public ErrorCodeGeneratorException(string message) : base(message) { }
+
+        public ErrorCodeGeneratorException(string message, Exception inner) : base(message, inner) { }
+    }
+}
